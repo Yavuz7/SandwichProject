961e7befde80a01f8404b0fa3aac761c8a3caf68
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-05-13 20:09:06 -0400

    Added react-navigation

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./Components/General/Header";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Header />
+        <StatusBar style="auto" hidden={hidden} />
+      </View>
+    </NavigationContainer>
   );
 }
 
